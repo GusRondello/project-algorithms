eece045253bdeb73dda1a3fdf5596ab0d49ea7e1
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-25 16:32:06 -0300

    feat: challenge_study_schedule.py implementada

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    p = len(permanence_period)
+    count = 0
+    if target_time == 0 or target_time is None:
+        return None
+
+    for index in range(0, p):
+        login = permanence_period[index][0]
+        logout = permanence_period[index][1]
+        if not type(login) == int or not type(logout) == int:
+            return None
+        elif login <= target_time <= logout:
+            count += 1
+    return count
