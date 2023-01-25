7d80d4fc90b0eb3461e7131887f26aee61f2aea0
Gustavo Rondello <gustavorondello99@outlook.com>
2023-01-25 19:18:29 -0300

    test: test_encrypt.py implementada

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(
+        TypeError
+    ):  # se key e message não possuírem os tipos corretos
+        encrypt_message(10, 10)
+        encrypt_message(10, "ola")
+        encrypt_message("ola", "10")
+
+    assert (
+        encrypt_message("ola", -1) == "alo"
+    )  # se key não for um índice positivo válido de message
+    assert encrypt_message("nezuko", 3) == "zen_oku"  # Se key for ímpar
+    assert encrypt_message("nezuko", 4) == "ok_uzen"  # Se key for par
